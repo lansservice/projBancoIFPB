4d8e1daf8177d942c8192a9361e4ccda0b8869db
lansservice <leonardo@lansservice.com.br>
2012-05-30 15:11:21 -0300

    Primeira classe do projeto do banco com alguns metodos prontos e o inicio do java doc.

---
+++ projBancoIfpb/src/bancoifpb/classes/Conta.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+
+
+package bancoifpb.classes;
+
+/**
+ * @author Leonardo Costa Vieira
+ * @author Alessandro
+ *
+ */
+
+import java.math.BigDecimal;
+
+public class Conta {
+
+	/** * Numero da conta bancaria, aceitando somente valores inteiros. Tamanho do campo 05 digitos.*/
+	private String numConta;
+
+	/** * Numero do Digito Verificador da  conta bancaria, aceitando somente valores inteiros. Tamanho do campo 01 digito. */
+	private String numDigitoVerificador;
+
+	/**	 * Armazena o CPF do correntista em uma string com 14 digitos.	 */
+	private String numCPF;
+	
+	/**  * Armazena o CNPJ do correntista em uma string com  digitos. */
+	private String numCNPJ;
+
+	/** * Armazena o saldo da conta em um valor numero com operaçoes precisas */
+	private BigDecimal valorSaldo;
+	
+	
+	public Conta() {
+		this.numConta = "00000";
+		this.numDigitoVerificador = "0";
+		this.numConta = "0000";
+		this.numCPF = "000.000.000-00";
+		this.numCNPJ = "00.000.000/0000-00";
+		this.valorSaldo = valorSaldo.add(null);
+	}
+
+	/** * Retorna o numero da conta */
+	public String getNumConta() {
+		return numConta;
+	}
+
+
+	public Boolean setNumConta(String numConta) {
+		if (numConta!= null && numConta.length()<6) {
+			this.numConta = numConta;
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+
+	/** * Retorna o numero do digito verificador da conta */
+	public String getNumDigitoVerificador(){
+		return numDigitoVerificador;
+	}
+
+
+	public Boolean setNumDigitoVerificador(String numDigitoVerificador) {
+		if (numDigitoVerificador != null && numDigitoVerificador.length()<10) {
+			this.numDigitoVerificador = numDigitoVerificador;
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+
+	public String getNumCPF() {
+		return numCPF;
+	}
+
+
+	public Boolean setNumCPF(String numCPF) {
+		if (numCPF!= null && numCPF.length()<15) {
+			this.numCPF = numCPF;
+			return true;	
+		}else {
+			return false;
+		}
+	}
+
+
+	public String getNumCNPJ() {
+		return numCNPJ;
+	}
+
+
+	public void setNumCNPJ(String numCNPJ) {
+		this.numCNPJ = numCNPJ;
+	}
+
+
+	public BigDecimal getValorSaldo() {
+		return valorSaldo;
+	}
+
+
+	public void setValorSaldo(BigDecimal valorSaldo) {
+		this.valorSaldo = valorSaldo;
+	}
+
+	/**
+	 * 
+	 */
+
+	
+	
+
+}
